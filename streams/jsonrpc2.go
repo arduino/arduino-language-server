62e5552a246c267647c03b67c1703aee064008d2
Cristian Maglie <c.maglie@arduino.cc>
2021-02-16 19:50:39 +0100

    Improved logging of LSP messages

--- streams/jsonrpc2.go
+++ streams/jsonrpc2.go
@@ -1,9 +1,12 @@
 package streams
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"runtime/debug"
 
+	"github.com/bcmi-labs/arduino-language-server/lsp"
 	"github.com/fatih/color"
 	"github.com/sourcegraph/jsonrpc2"
 )
@@ -34,19 +37,62 @@ func jsonrpcLog(prefix string, req *jsonrpc2.Request, resp *jsonrpc2.Response, s
 		c = green
 	}
 	if resp != nil {
+		dec := jsonrpcLogDecodeResp(resp)
 		if req != nil {
-			log.Print(c.Sprintf(prefix+" ANSWER %s %v (%v)", req.Method, req.ID, resp.ID))
+			log.Print(c.Sprintf(prefix+" ANSWER %s %v (%v): %s", req.Method, req.ID, resp.ID, dec))
 		} else {
-			log.Print(c.Sprintf(prefix+" ANSWER UNBOUND (%v)", resp.ID))
+			log.Print(c.Sprintf(prefix+" ANSWER UNBOUND (%v): %s", resp.ID, dec))
 		}
 	} else if req != nil {
+		dec := jsonrpcLogDecodeReq(req)
 		if !req.Notif {
-			log.Print(c.Sprintf(prefix+" REQUEST %s %v", req.Method, req.ID))
+			log.Print(c.Sprintf(prefix+" REQUEST %s %v: %s", req.Method, req.ID, dec))
 		} else {
-			log.Print(c.Sprintf(prefix+" NOTIFICATION %s", req.Method))
+			log.Print(c.Sprintf(prefix+" NOTIFICATION %s: %s", req.Method, dec))
 		}
 	} else {
 		log.Print(green.Sprintf(prefix + " NULL MESSAGE"))
 		log.Print(string(debug.Stack()))
 	}
+}
+
+func jsonrpcLogDecodeReq(req *jsonrpc2.Request) string {
+	fmtString := func(s *string) string {
+		if s == nil {
+			return ""
+		}
+		return *s
+	}
+	fmtFloat := func(s *float64) float64 {
+		if s == nil {
+			return 0
+		}
+		return *s
+	}
+	switch req.Method {
+	case "$/progress":
+		var v lsp.ProgressParams
+		if err := json.Unmarshal(*req.Params, &v); err != nil {
+			return err.Error()
+		}
+		var begin lsp.WorkDoneProgressBegin
+		if json.Unmarshal(*v.Value, &begin) == nil {
+			return fmt.Sprintf("TOKEN=%s BEGIN %v %v", v.Token, begin.Title, fmtString(begin.Message))
+		}
+		var report lsp.WorkDoneProgressReport
+		if json.Unmarshal(*v.Value, &report) == nil {
+			return fmt.Sprintf("TOKEN=%s REPORT %v %v%%", v.Token, fmtString(report.Message), fmtFloat(report.Percentage))
+		}
+		var end lsp.WorkDoneProgressEnd
+		if json.Unmarshal(*v.Value, &end) == nil {
+			return fmt.Sprintf("TOKEN=%s END %v", v.Token, fmtString(end.Message))
+		}
+		return "UNKNOWN?"
+	default:
+		return ""
+	}
+}
+
+func jsonrpcLogDecodeResp(resp *jsonrpc2.Response) string {
+	return ""
 }
