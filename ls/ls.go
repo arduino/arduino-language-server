22c33aec6bbf183765d2cff6857ed34c2c81f8eb
Cristian Maglie <c.maglie@arduino.cc>
2024-02-06 13:48:54 +0100

    Allow jobs tuning via -jobs N flag

--- ls/ls.go
+++ ls/ls.go
@@ -77,6 +77,7 @@ type Config struct {
 	EnableLogging                   bool
 	SkipLibrariesDiscoveryOnRebuild bool
 	DisableRealTimeDiagnostics      bool
+	Jobs                            int
 }
 
 var yellow = color.New(color.FgHiYellow)
