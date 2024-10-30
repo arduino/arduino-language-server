b2c8536dbcf3d6cb47e8171ed4377015ea7ef78e
Artis Avotins <artis.avotins@gmail.com>
2024-10-30 14:11:25 +0200

    Add platform/darwin aware CLI config resolution

---
+++ utils/path.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os/user"
+	"path"
+	"runtime"
+)
+
+// package-level variables for mocking in tests
+var (
+	userCurrent = user.Current
+	getGOOS     = runtime.GOOS
+)
+
+// GetDefaultCliConfigPath returns the default path for the ArduinoCLI configuration file.
+func GetDefaultCliConfigPath() string {
+	if user, _ := userCurrent(); user != nil {
+		return path.Join(user.HomeDir, func() string {
+			switch getGOOS {
+			case "darwin":
+				return "Library/Arduino15"
+			default:
+				return ".arduino15"
+			}
+		}(), "arduino-cli.yaml")
+	}
+	return ""
+}
---
+++ utils/path_test.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"os/user"
+	"path"
+	"testing"
+)
+
+func TestGetDefaultCliConfigPath(t *testing.T) {
+	// Save original GOOS getter and restore after test
+	originalGetGOOS := getGOOS
+	defer func() { getGOOS = originalGetGOOS }()
+
+	tests := []struct {
+		name     string
+		goos     string
+		wantPath string
+		user     *user.User
+	}{
+		{
+			name:     "darwin path",
+			goos:     "darwin",
+			user:     &user.User{HomeDir: "/Users/test"},
+			wantPath: path.Join("/Users/test", "Library/Arduino15", "arduino-cli.yaml"),
+		},
+		{
+			name:     "linux path",
+			goos:     "linux",
+			user:     &user.User{HomeDir: "/home/test"},
+			wantPath: path.Join("/home/test", ".arduino15", "arduino-cli.yaml"),
+		},
+		{
+			name:     "windows path",
+			goos:     "windows",
+			user:     &user.User{HomeDir: "C:\\Users\\test"},
+			wantPath: path.Join("C:\\Users\\test", ".arduino15", "arduino-cli.yaml"),
+		},
+		{
+			name:     "nil user",
+			goos:     "linux",
+			user:     nil,
+			wantPath: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// mocks
+			getGOOS = tt.goos
+			userCurrent = func() (*user.User, error) {
+				return tt.user, nil
+			}
+
+			got := GetDefaultCliConfigPath()
+			if got != tt.wantPath {
+				t.Errorf("GetDefaultCliConfigPath() = %v, want %v", got, tt.wantPath)
+			}
+		})
+	}
+}
